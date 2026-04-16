5cbf9bfda72cdf757482f41aab7e38387d3b01d8
Adamay6 <douma.ayadi58@gmail.com>
2026-04-16 17:40:57 +0200

    Itération 5 : cas général pas finalisé

--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -61,4 +61,29 @@ public class GameTest {
 			game.roll(0);
 		assertEquals(24,game.score());
 	}
+	
+/*	@Test
+	void test_cas_general() {
+		game.roll(1);
+		game.roll(2);
+		game.roll(10);
+		game.roll(0);
+		game.roll(10);
+		game.roll(4);
+		game.roll(2);
+		game.roll(0);
+		game.roll(10);
+		game.roll(6);
+		game.roll(2);
+		game.roll(0);
+		game.roll(10);
+		game.roll(6);
+		game.roll(4);
+		game.roll(8);
+		game.roll(2);
+		game.roll(2);
+		game.roll(7);
+		assertEquals(122,game.score());
+	}
+*/
 }
