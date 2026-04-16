219467ede71130eaf896f48b04a365b5a2b17bee
Adamay6 <douma.ayadi58@gmail.com>
2026-04-16 17:04:59 +0200

    Itération 2 : cas 0

--- test/tdd/GameTest.java
+++ test/tdd/GameTest.java
@@ -18,4 +18,13 @@ public class GameTest {
 	void test_vide() {
 		assertEquals(0,game.score());
 	}
+	
+	@Test
+	void test_aucune_quille() {
+		for(int i = 0; i<20;i++) 
+			game.roll(0);
+		assertEquals(0,game.score());
+	}
+	
+	
 }
